c39aa26d524d27c1f58749c82c8b71e5a4283cd6
shajmin306 <157534142+shajmin306@users.noreply.github.com>
2024-03-22 21:32:35 +0000

    Added basic search engine

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function changeSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", changeSearchSubmit);
