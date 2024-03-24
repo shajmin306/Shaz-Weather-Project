45241b4bbcd83bde5287b5dbce0d6d896d8bdb32
shajmin306 <157534142+shajmin306@users.noreply.github.com>
2024-03-24 20:26:39 +0000

    Injected further weather data

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ function updateWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class= "temp-icon"/>`;
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
